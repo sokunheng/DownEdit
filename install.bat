7ffa457646c704181d906b792036bf5544e949ea
Beyond_Speed <snamheng@gmail.com>
2022-10-21 23:00:49 +0700

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,12 @@
+pip install pystyle
+pip install requests 
+pip install inquirer
+pip install colorama
+pip install moviepy
+pip install rich
+
+cls
+@echo off
+echo " Installed Library Successfully! Please close the script. " 
+pause
+Del install.bat
