cb2c3daef59e4cbc52dde9e5798bf3406f3dcd78
Beyond_Speed <snamheng@gmail.com>
2022-11-12 01:22:31 +0700

    Update

--- main.py
+++ main.py
@@ -135,7 +135,7 @@ while True:
                     #initialization
                     def __init__(self):
                         self.headers = {
-                            'user-agent': 'Mozilla/5.0 (Linux; Android 8.0; Pixel 2 Build/OPD3.170816.012) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Mobile Safari/537.36 Edg/87.0.664.66'
+                            'User-Agent': 'Mozilla/5.0 (Linux; Android 8.0; Pixel 2 Build/OPD3.170816.012) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Mobile Safari/537.36 Edg/87.0.664.66'
                             }
 
                         # capture all videos
