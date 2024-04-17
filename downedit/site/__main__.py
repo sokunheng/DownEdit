708bd0bfadf587195da6bc26ddc11b111caaad64
SokunHeng <snamheng@gmail.com>
2024-04-17 12:36:07 +0700

    update code

--- downedit/site/__main__.py
+++ downedit/site/__main__.py
@@ -1,5 +1,6 @@
 from pystyle import *
 from colorama import *
+
 from downedit.utils.common import *
 from downedit.site.douyin import __main__ as douyin
 from downedit.site.kuaishou import __main__ as kuaishou
@@ -29,7 +30,9 @@ def main():
             questions = [' Douyin', ' Tiktok', ' Kuaishou', ' Back']
 
             selected = tool_selector.select_menu(
-                message=f"{Fore.YELLOW}Select Tools{Fore.WHITE}", choices=questions)\
+                message=f"{Fore.YELLOW}Select Tools{Fore.WHITE}",
+                choices=questions
+            )
 
             menu_list = {
                 " Douyin": douyin.main,
@@ -41,10 +44,8 @@ def main():
             tool_selector.execute_menu(selected, menu_list)
 
         except Exception as e:
-            print(
-                f"{Fore.YELLOW}[Programs] {Fore.MAGENTA}[Error] {Fore.RED}{str(e[:80])}")
-            print(input(
-                f"\n{Fore.CYAN}[Programs] {Fore.YELLOW}[Status] {Fore.WHITE}Press enter to continue.."))
+            logger.error(f"{Fore.RED}{str(e[:80])}")
+            logger.info(input("Press enter to continue..."))
 
 
 if __name__ == "__main__":
