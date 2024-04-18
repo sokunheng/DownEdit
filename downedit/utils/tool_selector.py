fdc568e3d7c3ba8720359d1298a613b8fd87418b
SokunHeng <snamheng@gmail.com>
2024-04-18 23:47:38 +0700

    improved empty input

--- downedit/utils/tool_selector.py
+++ downedit/utils/tool_selector.py
@@ -7,7 +7,7 @@ from colorama import *
 
 from .logger import Logger 
 
-logger = Logger("Program")
+logger = Logger("Programs")
 
 class ToolSelector:
     def __init__(self) -> None:
@@ -36,16 +36,15 @@ class ToolSelector:
 
     def execute_menu(self, selected, functions):
         if selected in functions:
-            
             if selected == " Back":
                 self.running = False
                 return
-            
             functions[selected]()
 
     def site_manual_select(self, banner, message):
         self.display_banner(banner=banner, message=message)
         choices = [ ' Single User', ' Batch Users', ' Exit' ]
+        
         question = self.select_menu(
             message=f"{Fore.YELLOW}Select Option{Fore.WHITE}",
             choices=choices
@@ -53,19 +52,22 @@ class ToolSelector:
         return question
 
     def single_user_select(self, banner, message):
-
         self.display_banner(banner=banner, message=message)
         user_input = input(f"{Fore.YELLOW}Enter User:{Fore.WHITE} ")
-        if user_input is None:
+        
+        if not user_input:
             logger.error("Please Enter Username!")
+            return
         return user_input
 
     def batch_user_select(self, banner, message):
-
         self.display_banner(banner=banner, message=message)
         file_path = input(f"{Fore.YELLOW}Enter File Path:{Fore.WHITE} ")
-        if file_path is None:
+        
+        if not file_path:
             logger.error("Please Enter File Path!")
+            return
+        
         try:
             with open(file_path, 'r') as file:
                 users = file.readlines()
