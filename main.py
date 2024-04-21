7fde6ce8b37c831f28b4b89061fe86c3a76dd4a8
SokunHeng <snamheng@gmail.com>
2024-04-21 13:42:35 +0700

    init

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import os
 import sys
-import platform
 
 from downedit.utils.logger import Logger
+from downedit.utils.system import SystemInfo
 
 logger = Logger("Programs")
+system_info = SystemInfo()
+pc_info = system_info.get_pc_info()
 
 try:
-    import wmi
-    import psutil
-
     from pystyle import *
     from colorama import *
 
@@ -27,37 +26,16 @@ except ImportError as e:
     logger.info(input("Press enter to continue..."))
 
 
-def get_pc_cpu():
-    cpu_info = wmi.WMI().Win32_Processor()[0]
-    cpu_name = cpu_info.Name
-    cpu_cores = os.cpu_count()
-    
-    return f"{cpu_name} ({cpu_cores} cores)"
-
-def pc_info():
-    pc_os = platform.system() + " " + platform.release() + f" ({platform.architecture()[0]})"
-    pc_user = platform.node()
-    pc_cpu = get_pc_cpu()
-    pc_ram = psutil.virtual_memory().total / (1024**3)
-    gpu_info = wmi.WMI().Win32_VideoController()[0]
-    pc_gpu = gpu_info.Description if gpu_info.Description else "No GPU found"
-
-    return pc_os, pc_user, pc_cpu, pc_ram, pc_gpu
-
 def display_banner():
-    pc_os, pc_user, pc_cpu, pc_ram, pc_gpu = pc_info()
-
     banner_display = f"""
     {Fore.MAGENTA}██████╗░███████╗{Back.RESET}  {Back.RED}{Fore.BLACK}sokunheng@GitHub - DownEdit v{DE_VERSION}{Fore.RESET}{Back.RESET}
     {Fore.MAGENTA}██╔══██╗██╔════╝{Back.RESET}  {Fore.WHITE}--------------------------{Back.RESET}
-    {Fore.MAGENTA}██║░░██║█████╗░░{Back.RESET}  {Fore.CYAN}OS:  {Fore.YELLOW}{pc_os}, {pc_user}{Fore.RESET}
-    {Fore.MAGENTA}██║░░██║██╔══╝░░{Back.RESET}  {Fore.CYAN}CPU: {Fore.YELLOW}{pc_cpu}{Fore.RESET}
-    {Fore.MAGENTA}██████╔╝███████╗{Back.RESET}  {Fore.CYAN}RAM: {Fore.YELLOW}{pc_ram:.3f} GB{Fore.RESET}
-    {Fore.MAGENTA}╚═════╝░╚══════╝{Back.RESET}  {Fore.CYAN}GPU: {Fore.YELLOW}{pc_gpu}{Fore.RESET}
+    {Fore.MAGENTA}██║░░██║█████╗░░{Back.RESET}  {Fore.CYAN}OS:  {Fore.YELLOW}{pc_info["OS"]}, {pc_info["USER"]}{Fore.RESET}
+    {Fore.MAGENTA}██║░░██║██╔══╝░░{Back.RESET}  {Fore.CYAN}CPU: {Fore.YELLOW}{pc_info["CPU"]}{Fore.RESET}
+    {Fore.MAGENTA}██████╔╝███████╗{Back.RESET}  {Fore.CYAN}RAM: {Fore.YELLOW}{pc_info["RAM"]:.3f} GB{Fore.RESET}
+    {Fore.MAGENTA}╚═════╝░╚══════╝{Back.RESET}  {Fore.CYAN}GPU: {Fore.YELLOW}{pc_info["GPU"]}{Fore.RESET}
     """
-            
     banner_msg = """Use arrow key to select the options"""
-    
     return banner_display, banner_msg
 
 
@@ -66,39 +44,26 @@ def main():
         tool_selector.running = True
         try:
             banner_display, banner_msg = display_banner()
-            
             tool_selector.display_banner(
                 banner_display,
                 banner_msg, title=" - Main Menu"
             )
             
-            choices = [
-                ' Edit Video',
-                f' AI Edit Video {Fore.RED}(Soon)',
-                ' Edit Photo',
-                ' AI Edit Photo',
-                ' Download Video',
-                ' AI-Generative Image',
-                f' AI-Generative Video {Fore.RED}(Soon)',
-                ' Exit'
-            ]
-
             menu_list = {
                 " Edit Video": video_edit.main,
-                " AI Edit Video (Soon)": lambda: None,
+                f" AI Edit Video {Fore.RED}(Soon)": lambda: None,
                 " Edit Photo": img_editor.main,
                 " AI Edit Photo": ai_img_editor.main,
                 " Download Video": vid_dl.main,
                 " AI-Generative Image": gen_img_ai.main,
-                " AI-Generative Video (Soon)": lambda: None,
+                f" AI-Generative Video {Fore.RED}(Soon)": lambda: None,
                 " Exit": lambda: sys.exit(0)
             }
-
+            
             selected = tool_selector.select_menu(
                 message=f"{Fore.YELLOW}Select Tools{Fore.WHITE}",
-                choices=choices
+                choices=list(menu_list.keys())
             )
-            
             tool_selector.execute_menu(
                 selected,
                 menu_list
