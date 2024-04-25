9380ae447611ef7bbc3d803f8aaf1207fac95737
SokunHeng <snamheng@gmail.com>
2024-04-26 00:18:48 +0700

    init

--- downedit/utils/tool_selector.py
+++ downedit/utils/tool_selector.py
@@ -4,14 +4,67 @@ import inquirer
 from colorama import Fore
 from pystyle import *
 from colorama import *
+from abc import ABC, abstractmethod
 
 from .logger import Logger 
 
 logger = Logger("Programs")
 
+class UserInput(ABC):
+    def __init__(self) -> None:
+        pass
+    
+    @abstractmethod
+    def get_user_input(self, message):
+        pass
+
+    @abstractmethod
+    def select_input(self, tool_selector, banner, message):
+        pass  
+
+class SingleInput(UserInput):
+    def get_user_input(self, message):
+        return input(message)
+    
+    def select_input(self, tool_selector, banner, message):
+        tool_selector.display_banner(banner, message)
+        user_input = self.get_user_input(f"{Fore.YELLOW}Enter User:{Fore.WHITE} ")
+        if not user_input:
+            logger.error("Please Enter Username!")
+            return
+        return user_input
+
+class BatchInput(UserInput):
+    def get_user_input(self, message):
+        return input(message)
+
+    def select_input(self, tool_selector, banner, message):
+        tool_selector.display_banner(banner, message)
+        file_path = self.get_user_input(f"{Fore.YELLOW}Enter File Path:{Fore.WHITE} ")
+        if not file_path:
+            logger.error("Please Enter File Path!")
+            return
+        try:
+            with open(file_path, 'r') as file:
+                lines = file.readlines()
+                lines = [line.strip() for line in lines]
+                return lines
+        except FileNotFoundError:
+            logger.error("File not found or could not be opened.")
+            return []
+
 class ToolSelector:
     def __init__(self) -> None:
         self.running = True
+        self._user_input = None
+    
+    def set_user_input(self, selection: UserInput):
+        self._user_input = selection
+    
+    def get_user_input(self, message):
+        if self._user_input is None:
+            raise ValueError("User input strategy not set")
+        return self._user_input.get_user_input(message)
     
     def show_box(self, message):
         print(f'{Fore.GREEN}')
@@ -39,40 +92,43 @@ class ToolSelector:
             if selected == " Back":
                 self.running = False
                 return
-            functions[selected]()
+            functions[selected].execute()
 
-    def site_manual_select(self, banner, message):
+    def manual_select(
+        self,
+        menu_options: dict,
+        banner,
+        message,
+    ):
         self.display_banner(banner=banner, message=message)
-        choices = [ ' Single User', ' Batch Users', ' Exit' ]
-        
         question = self.select_menu(
             message=f"{Fore.YELLOW}Select Option{Fore.WHITE}",
-            choices=choices
+            choices=menu_options
         )
         return question
+    
+    def selection_input(
+        self,
+        strategy: UserInput,
+        banner,
+        message
+    ):
+        return strategy.select_input(self, banner, message)
+        
+    def start(
+        self,
+        menu_options: dict,
+        input_message: str,
+    ) -> None:
+        """
+        Starts the tool selection process.
+        """
+        chosen_tool = self.select_menu(
+            message=input_message,
+            choices=list(menu_options.keys())
+        )
+        self.execute_menu(
+            chosen_tool,
+            menu_options
+        )
 
-    def single_user_select(self, banner, message):
-        self.display_banner(banner=banner, message=message)
-        user_input = input(f"{Fore.YELLOW}Enter User:{Fore.WHITE} ")
-        
-        if not user_input:
-            logger.error("Please Enter Username!")
-            return
-        return user_input
-
-    def batch_user_select(self, banner, message):
-        self.display_banner(banner=banner, message=message)
-        file_path = input(f"{Fore.YELLOW}Enter File Path:{Fore.WHITE} ")
-        
-        if not file_path:
-            logger.error("Please Enter File Path!")
-            return
-        
-        try:
-            with open(file_path, 'r') as file:
-                users = file.readlines()
-                users = [user.strip() for user in users]
-                return users
-        except FileNotFoundError:
-            logger.error("File not found or could not be opened.")
-            return []
