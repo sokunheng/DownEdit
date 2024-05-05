a733a706a7c7a6efe0d2dd5dd7493b68476a507d
SokunHeng <snamheng@gmail.com>
2024-05-05 18:15:35 +0700

    Add Config Class

--- downedit/__config__.py
+++ downedit/__config__.py
@@ -1,6 +1,14 @@
 CHUNK_SIZE = 1024
 DE_VERSION = open('version', 'r').read().strip()
-            
+
+class Config:
+    """
+    A class that provides setting for different types of configurations.
+    """
+    def __init__(self) -> None:
+        pass
+
+
 class EditFolder:
     """
     A class that provides constants for different types of edits.
@@ -10,7 +18,7 @@ class EditFolder:
         self.EDITED_IMG = "Edited_Photo"
         self.AI_Photo_Gen = "AI_Art"
         self.AI_Photo_Editor = "AI_Editor"
-        
+
 
 class Media:
     """
