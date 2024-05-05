ec6a7f1644cb3ef87a3504cc8001a3b94573ac52
SokunHeng <snamheng@gmail.com>
2024-05-06 01:02:58 +0700

    init

--- downedit/edit/video/__main__.py
+++ downedit/edit/video/__main__.py
@@ -5,29 +5,33 @@ from enum import Enum
 from colorama import *
 from pystyle import *
 
-from ...utils.common import *
+from ._editor import VideoEditor
 from ._process import *
+from ...utils.common import *
+from ...utils.file_utils import FileUtil
 
 
-class Tools(Enum):
-    FLIP_HORIZONTAL = 1
-    CUSTOM_SPEED = 2
-    FLIP_AND_SPEED = 3
-    ADD_MUSIC = 4
-    SPEED_AND_MUSIC = 5
-    FLIP_SPEED_MUSIC = 6
+def get_speed_factor(tool):
+    if tool in ["Custom Speed", "Flip + Speed", "Speed + Music", "Flip + Speed + Music"]:
+        return eval(input(f"{Fore.WHITE}[{Fore.MAGENTA}?{Fore.WHITE}] {Fore.YELLOW}Select Speed:{Fore.WHITE} "))
+    return 1.0
 
+def get_music_path(tool):
+    if tool in ["Add Music", "Speed + Music", "Flip + Speed + Music"]:
+        return input(f"{Fore.YELLOW}Enter Music:{Fore.WHITE} ")
+    return tool
 
-TOOL_TO_DIRECTORY = {
-    Tools.FLIP_HORIZONTAL: "Flip",
-    Tools.CUSTOM_SPEED: "Speed",
-    Tools.FLIP_AND_SPEED: "Flip_Speed",
-    Tools.ADD_MUSIC: "Add_Music",
-    Tools.SPEED_AND_MUSIC: "Speed_Music",
-    Tools.FLIP_SPEED_MUSIC: "Flip_Speed_Music"
-}
+def start_process(
+    tool: str,
+    video_speed: float,
+    music_path: str,
+    video_preset: str,
+    cpu_threads: int,
+    process_folder: str
+):
+    pass
 
-
+    
 def display_banner():
     banner_display = f"""{Fore.MAGENTA}
 ███████╗██████╗░██╗████████╗  ██╗░░░██╗██╗██████╗░███████╗░█████╗░
@@ -42,79 +46,62 @@ def display_banner():
     return banner_display, banner_msg
 
 
-def get_speed_factor(tool):
-    if tool in ["Custom Speed", "Flip And Speed", "Speed And Music", "Flip Speed Music"]:
-        return eval(input(f"{Fore.WHITE}[{Fore.MAGENTA}?{Fore.WHITE}] {Fore.YELLOW}Select Speed:{Fore.WHITE} "))
-    return 1.0
-
-
-def get_music_path(tool):
-    if tool in ["Add Music", "Speed And Music", "Flip Speed Music"]:
-        return input(f"{Fore.YELLOW}Enter Music:{Fore.WHITE} ")
-    return None
-
-
-def get_preset_from_answer(answer):
-
-    presets_mapping = {
-        ' Ultrafast': 'ultrafast',
-        ' Superfast': 'superfast',
-        ' Veryfast': 'veryfast',
-        ' Faster': 'faster',
-        ' Fast': 'fast',
-        ' Medium': 'medium',
-        ' Slow': 'slow',
-    }
-    return presets_mapping.get(answer, 'medium')
-
-
 def main():
-
-    Common.ensure_or_create_directory(EDITED_VIDEO)
-
+    max_cpu_cores = multiprocessing.cpu_count()
     banner_display, banner_msg = display_banner()
     tool_selector.display_banner(banner_display, banner_msg)
-
-    video_folder = input(f"{Fore.YELLOW}Enter folder:{Fore.WHITE} ")
-    print()
-
-    if not os.path.exists(video_folder):
-        console.log("[red][Folder][/red] No such directory")
-        time.sleep(0.5)
-        print(input(
-            f"{Fore.CYAN}[Programs] {Fore.YELLOW}[Status] {Fore.WHITE}Press enter to continue.."))
-        return
-
-    tools_choices = [
-        tool.name.replace("_", " ").title() for tool in Tools
+    available_tools = { 
+        " Flip Horizontal": lambda: None,
+        " Custom Speed": lambda: None,
+        " Loop Video": lambda: None,
+        " Flip + Speed": lambda: None,
+        " Add Music": lambda: None,
+        " Speed + Music": lambda: None,
+        " Flip + Speed + Music": lambda: None,
+    }
+    video_presets = {
+        " Ultrafast": "ultrafast",
+        " Superfast": "superfast",
+        " Veryfast": "veryfast",
+        " Faster": "faster",
+        " Fast": "fast",
+        " Medium": "medium",
+        " Slow": "slow",
+    }
+    cpu_cores_choices = [
+        str(i) for i in range(
+            1,
+            max_cpu_cores + 1
+        )
     ]
-
-    speed_choices = [' Ultrafast', ' Superfast', ' Veryfast', ' Faster',
-                     ' Fast', ' Medium', ' Slow']
-
-    max_cpu_cores = multiprocessing.cpu_count()
-    cpu_cores_choices = [str(i) for i in range(1, max_cpu_cores + 1)]
-
-    tool_answer = tool_selector.select_menu(
-        message=f"{Fore.YELLOW}Select Tools{Fore.WHITE}", choices=tools_choices)
-    speed_answer = tool_selector.select_menu(
-        message=f"{Fore.YELLOW}Process Speed{Fore.WHITE}", choices=speed_choices)
-    cpu_answer = tool_selector.select_menu(
-        message=f"{Fore.YELLOW}CPU Threads (Max: {max_cpu_cores}){Fore.WHITE}", choices=cpu_cores_choices)
-
-    selected_tool = Tools[tool_answer.replace(" ", "_").upper()]
-
-    preset = get_preset_from_answer(speed_answer)
-
-    output_folder = Common.ensure_or_create_directory(
-        directory_name=os.path.join(".", EDITED_VIDEO, TOOL_TO_DIRECTORY[selected_tool]))
-
-    speedf = get_speed_factor(tool_answer)
-    music_path = get_music_path(tool_answer)
-
-    RenderVideo.process_videos(video_folder=video_folder, output_folder=output_folder,
-                               process_function=selected_tool.value, speed_factor=speedf, music_path=music_path, threads=int(cpu_answer), preset=preset)
-
+    user_folder = FileUtil.validate_folder(input(f"{Fore.YELLOW}Enter folder:{Fore.WHITE} "))
+    selected_tool = tool_selector.select_menu(
+        message=f"{Fore.YELLOW}Choose Tools{Fore.WHITE}", 
+        choices=available_tools
+    )
+    video_speed = get_speed_factor(
+        selected_tool
+    )
+    music_path = get_music_path(
+        selected_tool
+    )
+    selected_presets = tool_selector.select_menu(
+        message=f"{Fore.YELLOW}Video Preset{Fore.WHITE}",
+        choices=video_presets
+    )
+    selected_threads = tool_selector.select_menu(
+        message=f"{Fore.YELLOW}CPU Threads (Max: {max_cpu_cores}){Fore.WHITE}", 
+        choices=cpu_cores_choices
+    )
+    start_process(
+        selected_tool,
+        video_speed,
+        music_path,
+        selected_presets,
+        selected_threads,
+        user_folder
+    )
+    
 
 if __name__ == "__main__":
     main()
