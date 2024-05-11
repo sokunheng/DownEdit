46d3b8c84d2608022a4553c1c867683e3ffd703a
SokunHeng <snamheng@gmail.com>
2024-05-12 03:35:02 +0700

    init

--- downedit/edit/video/_task.py
+++ downedit/edit/video/_task.py
@@ -1,16 +1,9 @@
-
-
 from ..task import Task
-from ._editor import VideoEditor
-from ._operation import VideoOperation
 
 
 class VideoTask(Task):
     """
     Task to be performed on video editor based on the selected operation.
     """
-    def __init__(self, editor: VideoEditor):
-        self.editor = editor
-        
-    def execute(self, operations: dict, threads, preset): ...
-        
+    def __init__(self) -> None: ...
+    def execute(self): ...
