f51c6fba9b393fe1d5fcefea4074d5eeae616350
SokunHeng <snamheng@gmail.com>
2024-05-23 22:53:58 +0700

    init

---
+++ downedit/edit/image/_task.py
@@ -0,0 +1,9 @@
+from ..task import Task
+
+
+class ImageTask(Task):
+    """
+    Task to be performed on image editor based on the selected operation.
+    """
+    def __init__(self) -> None: ...
+    def execute(self): ...
