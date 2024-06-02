8d3824d80d5cd874b0fcf000f654eaea602bfcae
SokunHeng <snamheng@gmail.com>
2024-06-02 22:22:44 +0700

    init

--- downedit/__init__.py
+++ downedit/__init__.py
@@ -1,2 +1,5 @@
-
+__author__ = "sokunheng"
 __version__ = "2.3.1"
+__description_en__ = "Download, Edit, and Generate Videos, Images and Sounds, in bulk using AI"
+__reponame__ = "DownEdit"
+__repourl__ = "https://github.com/sokunheng/DownEdit"
