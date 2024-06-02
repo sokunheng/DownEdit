f3b165480fa2d5f9e433dea48592ee7218d8946a
SokunHeng <snamheng@gmail.com>
2024-06-02 17:33:12 +0700

    init

--- downedit/edit/image/_process.py
+++ downedit/edit/image/_process.py
@@ -1,24 +1,37 @@
+import os
 import time
 
 from colorama import Fore
 
 from ...utils.logger import Logger
 from ...utils.file_utils import FileUtil
+from ._editor import ImageEditor
+from ._operation import (
+    ImageOperation,
+    Flip,
+    Crop,
+    Enhance,
+    Rotate,
+    Resize,
+    GrayScale,
+    Sharpen,
+    Blur
+)
+
 
 class ImageProcess:
     def __init__(
         self,
         tool: str,
-        adjust_degrees: int,
-        img_width: int,
-        img_height: int,
         process_folder: str,
-        output_folder: str
+        output_folder: str,
+        **kwargs
     ) -> None:
         self._tool = tool
-        self._adjust_degrees = adjust_degrees
-        self._img_width = img_width
-        self._img_height = img_height
+        self._adjust_degrees = kwargs.get("Degrees", 0)
+        self._img_width = kwargs.get("Width", 0)
+        self._img_height = kwargs.get("Height", 0)
+        self._blur_radius = kwargs.get("Radius", 1)
         self._input_folder = process_folder
         self._output_folder = output_folder
         self.logger = Logger("Programs")
@@ -56,4 +69,46 @@ class ImageProcess:
         limit_file_name = str(f'{file_name:60.60}')
         output_file_path = ""
         output_suffix = "" 
+        
+        # Execute the operations and build the suffix
+        image_editor = ImageEditor(image, output_file_path)
+        output_suffix = self._build_and_apply_operations(image_editor, output_suffix)
+        
+        # Construct the output file path with filename, suffix, and extension
+        output_file_path = FileUtil.get_output_file(
+            self._output_folder,
+            f"{file_name}{output_suffix}",
+            file_extension
+        )
+        
+        if os.path.exists(output_file_path):
+            self.logger.file_error(
+                f"Output file already exists - {limit_file_name}{output_suffix}{file_extension}"
+            )
+            return False
+        
+        self.logger.file_info(
+            f"Processing: [green]{limit_file_name}[/green]"
+        )
+        # Set the final output path
+        image_editor.output_path = output_file_path
+        # Save the image
+        image_editor.render()
         return True
+    
+    def _build_and_apply_operations(self, editor: ImageEditor, suffix: str) -> str:
+        """
+        Build the suffix based on the operations and apply them to the editor.
+        
+        Args:
+            editor (ImageEditor): The image editor.
+            suffix (str): The current suffix.
+            
+        Returns:
+            str: The updated suffix.
+        """
+        operations = self._get_operations()
+        for operation in operations:
+            suffix += operation.suffix
+            operation.function(editor)
+        return suffix
