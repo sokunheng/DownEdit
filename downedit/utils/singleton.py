c1d20a52f30523a8682925c7cd6a09598973b68c
SokunHeng <snamheng@gmail.com>
2024-06-03 01:10:29 +0700

    init

--- downedit/utils/singleton.py
+++ downedit/utils/singleton.py
@@ -1,25 +1,25 @@
 import threading
 from typing import Any, Dict, Tuple
 
-class Singleton(type):
+class SingletonMeta(type):
     """
     A metaclass that implements the Singleton pattern.
     Allows multiple instances if they are created with different constructor arguments.
     """
-    
+
     def __init__(cls, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        cls._instances: Dict[Tuple[Any, ...], Any] = {}
-        cls._lock: threading.Lock = threading.Lock()
+        cls._instance_cache: Dict[Tuple[Any, ...], Any] = {}
+        cls._instance_lock: threading.Lock = threading.Lock()
 
     def __call__(cls, *args, **kwargs):
         """
         Overrides the default class instantiation method.
         Creates a new instance if no matching instance exists, otherwise returns the existing one.
         """
-        key = (cls, args, frozenset(kwargs.items()))
-        with cls._lock:
-            if key not in cls._instances:
+        inst_key = (cls, args, frozenset(kwargs.items()))
+        with cls._instance_lock:
+            if inst_key not in cls._instance_cache:
                 instance = super().__call__(*args, **kwargs)
-                cls._instances[key] = instance
-        return cls._instances[key]
+                cls._instance_cache[inst_key] = instance
+        return cls._instance_cache[inst_key]
