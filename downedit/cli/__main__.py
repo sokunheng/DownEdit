709799c01c2970ea18bd59874f7ab8369f63f246
SokunHeng <snamheng@gmail.com>
2024-06-05 00:19:54 +0700

    Update menu options for sound editing

--- downedit/cli/__main__.py
+++ downedit/cli/__main__.py
@@ -15,8 +15,9 @@ try:
     from .menu.media import media as vid_dl
     from .menu.images import ai_generator as gen_img_ai
     from .menu.images import ai_editor as ai_img_editor
-    from .menu.images import editor as img_editor
-    from .menu.videos import editor as video_edit
+    from .menu.images import editor as image_editor
+    from .menu.videos import editor as video_editor
+    from .menu.sounds import editor as sound_editor
     from ..__config__ import DE_VERSION
     from ..utils.common import tool_selector
     
@@ -30,7 +31,7 @@ def display_banner():
     banner_display = f"""
     {Fore.MAGENTA}██████╗░███████╗{Back.RESET}  {Back.RED}{Fore.BLACK}sokunheng@GitHub - DownEdit v{DE_VERSION}{Fore.RESET}{Back.RESET}
     {Fore.MAGENTA}██╔══██╗██╔════╝{Back.RESET}  {Fore.WHITE}--------------------------{Back.RESET}
-    {Fore.MAGENTA}██║░░██║█████╗░░{Back.RESET}  {Fore.CYAN}OS:  {Fore.YELLOW}{pc_info["OS"]}, {pc_info["USER"]}{Fore.RESET}
+    {Fore.MAGENTA}██║░░██║█████╗░░{Back.RESET}  {Fore.CYAN}OS : {Fore.YELLOW}{pc_info["OS"]}, {pc_info["USER"]}{Fore.RESET}
     {Fore.MAGENTA}██║░░██║██╔══╝░░{Back.RESET}  {Fore.CYAN}CPU: {Fore.YELLOW}{pc_info["CPU"]}{Fore.RESET}
     {Fore.MAGENTA}██████╔╝███████╗{Back.RESET}  {Fore.CYAN}RAM: {Fore.YELLOW}{pc_info["RAM"]:.3f} GB{Fore.RESET}
     {Fore.MAGENTA}╚═════╝░╚══════╝{Back.RESET}  {Fore.CYAN}GPU: {Fore.YELLOW}{pc_info["GPU"]}{Fore.RESET}
@@ -50,11 +51,11 @@ def run():
             )
             available_tools = {
                 " Download Video": vid_dl.main,
-                " Edit Video": video_edit.main,
+                " Edit Video": video_editor.main,
                 f" AI Edit Video {Fore.RED}(Soon)": lambda: None,
-                " Edit Photo": img_editor.main,
+                " Edit Photo": image_editor.main,
                 " AI Edit Photo": ai_img_editor.main,
-                f" Edit Sound {Fore.RED}(Soon)": lambda: None,
+                f" Edit Sound": sound_editor.main,
                 f" AI Edit Sound {Fore.RED}(Soon)": lambda: None,
                 " AI-Generative Image": gen_img_ai.main,
                 f" AI-Generative Video {Fore.RED}(Soon)": lambda: None,
@@ -68,7 +69,7 @@ def run():
             
         except Exception as e:
             logger.error(str(e[:80]))
-            logger.info(input("Press enter to continue..."))    
+            logger.keybind(f"{Fore.GREEN}Press enter to continue...")
 
         except KeyboardInterrupt as e:
             logger.info("Skipping the process..")
