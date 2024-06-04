cb70bf0da243b3cc23764baa6025f6f0ff749158
SokunHeng <snamheng@gmail.com>
2024-06-05 00:14:49 +0700

    init

--- downedit/cli/menu/sounds/editor.py
+++ downedit/cli/menu/sounds/editor.py
@@ -1,42 +1,45 @@
-import os
-import multiprocessing
 import time
 
-from enum import Enum
-from colorama import *
-from pystyle import *
+from colorama import Fore
 
+from .._banners import get_banner
+from ....utils.common import tool_selector
+from ....utils.logger import Logger
 from ....utils.file_utils import FileUtil
-from ....utils.common import (
-    tool_selector,
-    logger,
-    DE_VERSION
-)
+from ....edit.sound._process import SoundProcess
 
-    
-def display_banner():
-    banner_display = f"""{Fore.MAGENTA}
-███████╗██████╗░██╗████████╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
-██╔════╝██╔══██╗██║╚══██╔══╝  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
-█████╗░░██║░░██║██║░░░██║░░░  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
-██╔══╝░░██║░░██║██║░░░██║░░░  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
-███████╗██████╔╝██║░░░██║░░░  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
-╚══════╝╚═════╝░╚═╝░░░╚═╝░░░  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
-                Created by HengSok - v{DE_VERSION}
-    """
-    banner_msg = r"Example: C:\Users\Name\Desktop\Folder\Video"
-    return banner_display, banner_msg
+
+logger = Logger("Programs")
 
 
 def main():
-    banner_display, banner_msg = display_banner()
-    tool_selector.display_banner(banner_display, banner_msg)
-    user_folder = FileUtil.validate_folder(
-        input(f"{Fore.YELLOW}Enter folder:{Fore.WHITE} ")
-    )
-    time.sleep(0.5)
-    logger.info(input("Press enter to continue..."))
-
+    try:
+        banner_display, banner_msg = get_banner("SOUND_EDITOR")
+        tool_selector.display_banner(banner_display, banner_msg, "- Sound editor")
+        available_tools = SoundProcess.get_tools()
+        user_folder = FileUtil.validate_folder(
+            folder_path=input(f"{Fore.YELLOW}Enter folder:{Fore.WHITE} ")
+        )
+        selected_tool = tool_selector.select_menu(
+            message=f"{Fore.YELLOW}Choose Tools{Fore.WHITE}", 
+            choices=available_tools
+        )
+        sound_params = tool_selector.get_tool_input(
+            available_tools,
+            selected_tool
+        )
+        sound_process = SoundProcess(
+            tool=selected_tool,
+            process_folder=user_folder,
+            **sound_params
+        )
+        sound_process.start()
+        
+    except Exception as e:
+        logger.folder_error(e)
+        time.sleep(0.5)
+        logger.keybind(f"{Fore.GREEN}Press enter to continue...")
+        return
 
 if __name__ == "__main__":
     main()
