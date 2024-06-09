69036818c156c2d0e36e5320d3234c5b0e1cc1d3
SokunHeng <snamheng@gmail.com>
2024-06-10 00:49:53 +0700

    init

--- downedit/utils/logger.py
+++ downedit/utils/logger.py
@@ -1,138 +1,93 @@
+import logging
 import time
-import colorama
 
-from colorama import Fore
+from rich.logging import RichHandler
 from rich.traceback import install
-from rich.console import Console
 
 from .singleton import Singleton
 
 install()
-console = Console()
-console._log_render.omit_repeated_times = False
+
+class Formatter(logging.Formatter):
+    msg_color = {
+        "DEBUG"     : "cyan",
+        "INFO"      : "green",
+        "WARNING"   : "yellow",
+        "ERROR"     : "red",
+        "CRITICAL"  : "bold red",
+        "FILE"      : "magenta",
+    }
+
+    def __init__(self, fmt="{message}", style='{', datefmt="[%X]"):
+        super().__init__(fmt=fmt, datefmt=datefmt, style=style)
+
+    def format(self, record):
+        """
+        Format the message with color based on the log level
+        """
+        # Retrieve the original message
+        message = record.getMessage()
+        # color = self.msg_color.get(record.levelname, 'white')
+        # message = f"[{color}]{message}[/]"
+        record.msg = message
+        return super().format(record)
+    
 
 class Logger(metaclass=Singleton):
-    def __init__(self, name=None):
-        self.name = name
-    
-    def time(self, time, info, message):
-        """
-        Print a message with a timestamp.
-        
-        Args:
-            time (str): The timestamp.
-            info (str): The type of message.
-            message (str): The message to print.
-        
-        Returns:
-            None
-        """
-        print(
-            f"\n{Fore.CYAN}[{time}] {Fore.YELLOW}[{info}] {Fore.WHITE}",
-            message
-        )
-        
-    def info(self, message):
-        """
-        Print an information message.
-        
-        Args:
-            message (str): The message to print.
-        
-        Returns:
-            None
-        """
-        print(
-            f"\n{Fore.CYAN}[{self.name}] {Fore.YELLOW}[Status]{Fore.WHITE}",
-            message
-        )
+    def __init__(self):
+        # Prevent re-initialization of the logger
+        if hasattr(self, '_ready'):
+            return
 
-    def error(self, message):
+        self.prime_logger = logging.getLogger("DownEdit")
+        self.prime_logger.setLevel(logging.DEBUG)
+        self._ready = True
+
+    def config_log(self, log_level=logging.DEBUG):
         """
-        Print an error message.
+        Set new log level and configure 
+        """
+        self.prime_logger.handlers.clear()
+        self.prime_logger.setLevel(log_level)
         
-        Args:
-            message (str): The message to print.
-            
-        Returns:
-            None
-        """
-        print(
-            f"\n{Fore.CYAN}[{self.name}] {Fore.YELLOW}[Error]{Fore.RED}",
-            message
+        console_handler = RichHandler(
+            show_time=True,
+            show_path=False,
+            markup=True,
+            rich_tracebacks=True,
+            omit_repeated_times=False
         )
-    
-    def keybind(self, message):
-        """
-        Print a keybinding message.
-        
-        Args:
-            message (str): The message to print.
-            
-        Returns:
-            None
-        """
-        print(
-            input(
-                f"{Fore.CYAN}[Programs] {Fore.YELLOW}[Status] {Fore.WHITE}{message}"
-        ))
-    
-    def folder_info(self, message):
-        """
-        Print a folder information message.
-        
-        Args:
-            message (str): The message to print.
-            
-        Returns:
-            None
-        """
-        console.log(
-            "[red][FOLDER][/red]",
-            message
-        ) 
-        
-    def folder_error(self, message):
-        """
-        Print a folder error message.
-        
-        Args:
-            message (str): The message to print.
-        
-        Returns:
-            None
-        """
-        console.log(
-            "[red][FOLDER][/red]",
-            message
-        ) 
-        
-    def file_info(self, message):
-        """
-        Print a file information message.
-        
-        Args:
-            message (str): The message to print.
-        
-        Returns:
-            None
-        """
-        console.log(
-            "[cyan][FILE][/cyan]",
-            message
-        )
-        
-    def file_error(self, message):
-        """
-        Print a file error message.
-        
-        Args:
-            message (str): The message to print.
-        
-        Returns:
-            None
-        """
-        console.log(
-            "[red][FILE][/red]",
-            message
-        )
+        console_handler.setFormatter(Formatter())
+        self.prime_logger.addHandler(console_handler)
+
+    def close(self):
+        # Properly close and remove all handlers
+        for handler in self.prime_logger.handlers:
+            handler.close()
+            self.prime_logger.removeHandler(handler)
+        self.prime_logger.handlers.clear()
+        # Ensure all logs are processed before exit
+        time.sleep(1)
+
+
+# Adding custom log level method
+FILE_LEVEL_NUM = 25
+logging.addLevelName(FILE_LEVEL_NUM, "FILE")
+
+def file(self, message, *args, **kwargs):
+    if self.isEnabledFor(FILE_LEVEL_NUM):
+        self._log(FILE_LEVEL_NUM, message, args, **kwargs)
+
+logging.Logger.file = file
+
+
+def init_logging():
+    logger = logging.getLogger("DownEdit")
+    if not logger.handlers:
+        log_control = Logger()
+        log_control.config_log(log_level=logging.DEBUG)
+    return logger
+
+
+# Initialize the logger
+logger = init_logging()
