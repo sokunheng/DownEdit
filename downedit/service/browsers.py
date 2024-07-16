cda1435e058ce50116bdd6418acae301955decde
SokunHeng <snamheng@gmail.com>
2024-07-17 00:46:23 +0700

    init

---
+++ downedit/service/browsers.py
@@ -0,0 +1,189 @@
+class Chrome():
+    """
+    Google Chrome
+    
+    Description:
+        The version of Chrome is based on the WebKit version. We have used the WebKit version to determine the minor version range.
+    
+    Reference:
+        https://chromereleases.googleblog.com/search/label/Stable%20updates
+    """
+    def __init__(self):
+        pass
+    
+    def get_versions(self): 
+        return {
+            '98.0.4758': {'minor_range': (0, 255), 'webkit': '537.36'},
+            '99.0.4844': {'minor_range': (0, 255), 'webkit': '537.36'},
+            '100.0.4896': {'minor_range': (0, 255), 'webkit': '537.36'},
+            '101.0.4951': {'minor_range': (0, 255), 'webkit': '537.36'},
+            '102.0.5005': {'minor_range': (0, 255), 'webkit': '537.36'},
+            '103.0.5060': {'minor_range': (0, 255), 'webkit': '537.36'},
+            '104.0.5112': {'minor_range': (0, 255), 'webkit': '537.36'},
+            '105.0.5195': {'minor_range': (0, 255), 'webkit': '537.36'},
+            '106.0.5249': {'minor_range': (0, 255), 'webkit': '537.36'},
+            '107.0.5304': {'minor_range': (0, 255), 'webkit': '537.36'},
+            '108.0.5359': {'minor_range': (0, 255), 'webkit': '537.36'},
+            '109.0.5414': {'minor_range': (0, 255), 'webkit': '537.36'},
+            '110.0.5481': {'minor_range': (0, 255), 'webkit': '537.36'},
+            '111.0.5563': {'minor_range': (0, 255), 'webkit': '537.36'},
+            '112.0.5615': {'minor_range': (0, 255), 'webkit': '537.36'},
+            '114.0.5735': {'minor_range': (0, 255), 'webkit': '537.36'},
+            '115.0.5790': {'minor_range': (0, 255), 'webkit': '537.36'},
+            '116.0.5845': {'minor_range': (0, 255), 'webkit': '537.36'},
+            '117.0.5938': {'minor_range': (0, 255), 'webkit': '537.36'},
+            '118.0.5993': {'minor_range': (0, 255), 'webkit': '537.36'},
+            '119.0.6045': {'minor_range': (0, 255), 'webkit': '537.36'},
+            '120.0.6099': {'minor_range': (0, 255), 'webkit': '537.36'},
+            '121.0.6167': {'minor_range': (0, 255), 'webkit': '537.36'},
+            '122.0.6261': {'minor_range': (0, 255), 'webkit': '537.36'},
+            '123.0.6312': {'minor_range': (0, 255), 'webkit': '537.36'},
+            '124.0.6367': {'minor_range': (0, 255), 'webkit': '537.36'},
+            '125.0.6422': {'minor_range': (0, 255), 'webkit': '537.36'},
+            '126.0.6478': {'minor_range': (0, 0), 'webkit': '537.36'},
+        }
+
+class Firefox():
+    """
+    Mozilla Firefox
+    
+    description:
+        The version of Firefox is based on the Gecko version. We have used the stable release version provided by Mozilla to determine the minor version range.
+    
+    Reference:
+        https://www.mozilla.org/en-US/firefox/releases/
+    """
+    def __init__(self):
+        pass
+    
+    def get_versions(self): 
+        return {
+            '103.0': {'minor_range': (0, 2)},
+            '104.0': {'minor_range': (0, 2)},
+            '105.0': {'minor_range': (0, 3)},
+            '106.0': {'minor_range': (0, 5)},
+            '107.0': {'minor_range': (0, 1)},
+            '108.0': {'minor_range': (0, 2)},
+            '109.0': {'minor_range': (0, 1)},
+            '110.0': {'minor_range': (0, 1)},
+            '111.0': {'minor_range': (0, 1)},
+            '112.0': {'minor_range': (0, 2)},
+            '113.0': {'minor_range': (0, 2)},
+            '114.0': {'minor_range': (0, 2)},
+            '115.0': {'minor_range': (0, 3)},
+            '115.1': {'minor_range': (0, 0)},
+            '115.2': {'minor_range': (0, 1)},
+            '115.3': {'minor_range': (0, 1)},
+            '115.4': {'minor_range': (0, 0)},
+            '115.5': {'minor_range': (0, 0)},
+            '115.6': {'minor_range': (0, 0)},
+            '115.7': {'minor_range': (0, 0)},
+            '115.8': {'minor_range': (0, 0)},
+            '116.0': {'minor_range': (0, 3)},
+            '117.0': {'minor_range': (0, 1)},
+            '118.0': {'minor_range': (0, 2)},
+            '119.0': {'minor_range': (0, 1)},
+            '120.0': {'minor_range': (0, 1)},
+            '121.0': {'minor_range': (0, 1)},
+            '122.0': {'minor_range': (0, 1)},
+            '123.0': {'minor_range': (0, 1)},
+            '124.0': {'minor_range': (0, 2)},
+            '125.0': {'minor_range': (1, 3)},
+            '126.0': {'minor_range': (0, 0)},
+            '127.0': {'minor_range': (1, 2)},
+            '128.0': {'minor_range': (0, 0)},
+        }
+
+class Edge():
+    """
+    Microsoft Edge
+    
+    description:
+        The version of Edge is based on the WebKit version. We have used the WebKit version to determine the minor version range.
+    
+    Reference:
+        https://docs.microsoft.com/en-us/deployedge/microsoft-edge-release-schedule
+    """
+    def __init__(self):
+        pass
+    
+    def get_versions(self): 
+        return {
+            '98.0.1108': {'minor_range': (0, 99), 'webkit': '537.36'},
+            '99.0.1141': {'minor_range': (0, 99), 'webkit': '537.36'},
+            '99.0.1146': {'minor_range': (0, 99), 'webkit': '537.36'},
+            '100.0.1185': {'minor_range': (0, 99), 'webkit': '537.36'},
+            '101.0.1210': {'minor_range': (0, 99), 'webkit': '537.36'},
+            '102.0.1245': {'minor_range': (0, 99), 'webkit': '537.36'},
+            '103.0.1264': {'minor_range': (0, 99), 'webkit': '537.36'},
+            '104.0.1293': {'minor_range': (0, 99), 'webkit': '537.36'},
+            '105.0.1343': {'minor_range': (0, 99), 'webkit': '537.36'},
+            '106.0.1370': {'minor_range': (0, 99), 'webkit': '537.36'},
+            '107.0.1418': {'minor_range': (0, 99), 'webkit': '537.36'},
+            '108.0.1462': {'minor_range': (0, 99), 'webkit': '537.36'},
+            '109.0.1518': {'minor_range': (0, 99), 'webkit': '537.36'},
+            '110.0.1587': {'minor_range': (0, 99), 'webkit': '537.36'},
+            '111.0.1661': {'minor_range': (0, 99), 'webkit': '537.36'},
+            '112.0.1722': {'minor_range': (0, 99), 'webkit': '537.36'},
+            '113.0.1774': {'minor_range': (0, 99), 'webkit': '537.36'},
+            '114.0.1823': {'minor_range': (0, 99), 'webkit': '537.36'},
+            '115.0.1901': {'minor_range': (0, 99), 'webkit': '537.36'},
+            '116.0.1938': {'minor_range': (0, 99), 'webkit': '537.36'},
+            '117.0.2045': {'minor_range': (0, 99), 'webkit': '537.36'},
+            '118.0.2088': {'minor_range': (0, 99), 'webkit': '537.36'},
+            '119.0.2151': {'minor_range': (0, 99), 'webkit': '537.36'},
+            '120.0.2210': {'minor_range': (0, 99), 'webkit': '537.36'},
+            '121.0.2277': {'minor_range': (0, 99), 'webkit': '537.36'},
+            '122.0.2365': {'minor_range': (0, 99), 'webkit': '537.36'},
+            '123.0.2420': {'minor_range': (0, 99), 'webkit': '537.36'},
+            '124.0.2478': {'minor_range': (0, 99), 'webkit': '537.36'},
+            '125.0.2535': {'minor_range': (0, 99), 'webkit': '537.36'},
+            '126.0.2592': {'minor_range': (0, 99), 'webkit': '537.36'},
+            '127.0.2651': {'minor_range': (0, 99), 'webkit': '537.36'},
+        }
+
+class Safari():
+    """
+    Apple Safari
+    
+    description:
+        The version of Safari is based on the WebKit version. We have used the WebKit version to determine the minor version range.
+    
+    Reference:
+        https://developer.apple.com/documentation/safari-release-notes
+    """
+    def __init__(self):
+        pass
+    
+    def get_versions(self): 
+        return {
+            '8': {'minor_range': (0, 0), 'webkit': '600.5.17'},
+            '9': {'minor_range': (0, 0), 'webkit': '601.1.46'},
+            '10': {'minor_range': (0, 0), 'webkit': '602.4.8'},
+            '11': {'minor_range': (0, 0), 'webkit': '604.1.38'},
+            '12': {'minor_range': (0, 1), 'webkit': '605.1.15'},
+            '13': {'minor_range': (0, 1), 'webkit': '605.1.15'},
+            '14': {'minor_range': (0, 1), 'webkit': '605.1.15'},
+            '15': {'minor_range': (0, 6), 'webkit': '605.1.15'},
+            '16': {'minor_range': (0, 6), 'webkit': '605.1.15'},
+            '17': {'minor_range': (0, 5), 'webkit': '605.1.15'}
+        }
+    
+class Browser():
+    def __init__(self, browser):
+        self.browser = self.get_browser(browser)
+    
+    def get_browser(self, browser_name):
+        if browser_name.lower() == 'chrome':
+            return Chrome()
+        elif browser_name.lower() == 'firefox':
+            return Firefox()
+        elif browser_name.lower() == 'edge':
+            return Edge()
+        elif browser_name.lower() == 'safari':
+            return Safari()
+        else:
+            return Chrome()
+    
+    def get_browser_versions(self):
+        return self.browser.get_versions()
---
+++ downedit/service/platforms.py
@@ -0,0 +1,342 @@
+class Android():
+    """
+    Android mobile operating system
+    
+    description:
+        The version of Android is based on the API level. We have used the stable release version provided by Google to determine the minor version range.
+    
+    Reference:
+        https://source.android.com/setup/start/build-numbers
+    """
+    def __init__(self):
+        pass
+    
+    def get_versions(self): 
+        return {
+        '8.0': {
+            'minor_range': (0, 5),
+            'build_number': ('OPR1.{d}.{v}', 'OPR2.{d}.{v}', 'OPR3.{d}.{v}', 'OPR4.{d}.{v}', 'OPR5.{d}.{v}',
+                            'OPR6.{d}.{v}', 'OPD1.{d}.{v}', 'OPD2.{d}.{v}', 'OPD3.{d}.{v}')
+        },
+        '8.1': {
+            'minor_range': (0, 7),
+            'build_number': ('OPM1.{d}.{v}', 'OPM2.{d}.{v}', 'OPM3.{d}.{v}', 'OPM4.{d}.{v}', 'OPM5.{d}.{v}')
+        },
+        '9.0': {
+            'minor_range': (0, 0),
+            'build_number': ('PPR1.{d}.{v}', 'PPR2.{d}.{v}', 'PD1A.{d}.{v}', 'PQ1A.{d}.{v}', 'PQ2A.{d}.{v}',
+                            'PQ3A.{d}.{v}', 'PQ3B.{d}.{v}', 'QQ2A.{d}.{v}')
+        },
+        '10.0': {
+            'minor_range': (0, 0),
+            'build_number': ('QD1A.{d}.{v}', 'QQ1B.{d}.{v}', 'QQ1C.{d}.{v}', 'QQ1D.{d}.{v}', 'QQ2A.{d}.{v}')
+        },
+        '11.0': {
+            'minor_range': (0, 0),
+            'build_number': ('RP1A.{d}.{v}', 'RP1B.{d}.{v}', 'RP1C.{d}.{v}', 'RP1D.{d}.{v}', 'RD1A.{d}.{v}',
+                            'RD1B.{d}.{v}', 'RQAA.{d}.{v}', 'RQ3A.{d}.{v}', 'RQ1D.{d}.{v}')
+        },
+        '12.0': {
+            'minor_range': (0, 0),
+            'build_number': ('SP1A.{d}.{v}', 'SD1A.{d}.{v}', 'SQ1D.{d}.{v}', 'SQ1A.{d}.{v}', 'SQ1D.{d}.{v}')
+        },
+        '12.1': {
+            'minor_range': (0, 0),
+            'build_number': ('SP2A.{d}.{v}', 'SD2A.{d}.{v}', 'SQ3A.{d}.{v}')
+        },
+        '13.0': {
+            'minor_range': (0, 0),
+            'build_number': ('TQ3A.{d}.{v}', 'TQ2A.{d}.{v}', 'TP1A.{d}.{v}', 'TQ1A.{d}.{v}', 'TD1A.{d}.{v}')
+        },
+        '14.0': {
+            'minor_range': (0, 0),
+            'build_number': ('UP1A.{d}.{v}', 'UD1A.{d}.{v}', 'UQ1A.{d}.{v}')
+        },
+    }
+    
+    def get_models(self):
+        """
+        Samsung Galaxy Models
+        
+        description:
+            The list of Samsung Galaxy models that are supported by the service.
+        
+        Reference:
+            https://firmware.gem-flash.com/index.php?a=downloads&b=folder&id=980
+        """
+        return (
+            'SM-G390Y', 'SM-G390Y', 'SM-G525F', 'SM-G9006W', 'SM-G9209K', 'SM-316U',
+            'SM-318ML', 'SM-318MZ', 'SM-318MZ', 'SM-360GY', 'SM-G110B', 'SM-G110H',
+            'SM-G110M', 'SM-G130BT', 'SM-G130BU', 'SM-G130E', 'SM-G130H', 'SM-G130HN',
+            'SM-G130M', 'SM-G130U', 'SM-G150N0', 'SM-G150NK', 'SM-G150NL', 'SM-G150NS',
+            'SM-G155S', 'SM-G1600', 'SM-G1600', 'SM-G160N', 'SM-G1650', 'SM-G165N',
+            'SM-G30HN', 'SM-G310H', 'SM-G310HN', 'SM-G310R', 'SM-G310R5', 'SM-G3139',
+            'SM-G3139D', 'SM-G313F', 'SM-G313H', 'SM-G313HN', 'SM-G313HU', 'SM-G313HY',
+            'SM-G313HZ', 'SM-G313M', 'SM-G313ML', 'SM-G313MU', 'SM-G313MY', 'SM-G313U',
+            'SM-G316F', 'SM-G316HU', 'SM-G316M', 'SM-G316ML', 'SM-G316MY', 'SM-G318',
+            'SM-G318H', 'SM-G318HZ', 'SM-G318M', 'SM-G318ML', 'SM-G318MZ', 'SM-G350',
+            'SM-G3502', 'SM-G3502C', 'SM-G3502I', 'SM-G3502L', 'SM-G3502T', 'SM-G3502U',
+            'SM-G3508', 'SM-G3508I', 'SM-G3508J', 'SM-G3509', 'SM-G3509I', 'SM-G350E',
+            'SM-G350L', 'SM-G350M', 'SM-G350X', 'SM-G3518', 'SM-G3556D', 'SM-G3558',
+            'SM-G3559', 'SM-G355H', 'SM-G355HN', 'SM-G355HQ', 'SM-G355M', 'SM-G3568V',
+            'SM-G357', 'SM-G357FZ', 'SM-G357FZ', 'SM-G357M', 'SM-G3586V', 'SM-G3588V',
+            'SM-G3589W', 'SM-G3606', 'SM-G3608', 'SM-G3609', 'SM-G360AZ', 'SM-G360BT',
+            'SM-G360F', 'SM-G360F', 'SM-G360FY', 'SM-G360G', 'SM-G360GY', 'SM-G360H',
+            'SM-G360HU', 'SM-G360M', 'SM-G360P', 'SM-G360T', 'SM-G360V', 'SM-G361F',
+            'SM-G361H', 'SM-G361HU', 'SM-G368T', 'SM-G3812', 'SM-G3812B', 'SM-G3815',
+            'SM-G3818', 'SM-G3818ZM', 'SM-G3819D', 'SM-G3858', 'SM-G386F', 'SM-G386T',
+            'SM-G386T1', 'SM-G386U', 'SM-G386W', 'SM-G388F', 'SM-G389F', 'SM-G390F',
+            'SM-G390W', 'SM-G390Y', 'SM-G398FN', 'SM-G5108', 'SM-G5108Q', 'SM-G5109',
+            'SM-G5306W', 'SM-G5308W', 'SM-G5309W', 'SM-G530A', 'SM-G530AZ', 'SM-G530BT',
+            'SM-G530F', 'SM-G530FQ', 'SM-G530FZ', 'SM-G530H', 'SM-G530M', 'SM-G530MU',
+            'SM-G530P', 'SM-G530R4', 'SM-G530R7', 'SM-G530T', 'SM-G530T1', 'SM-G530W',
+            'SM-G530Y', 'SM-G530YZ', 'SM-G531BT', 'SM-G531F', 'SM-G531H', 'SM-G531M',
+            'SM-G531Y', 'SM-G532F', 'SM-G532G', 'SM-G532M', 'SM-G532MT', 'SM-G5500',
+            'SM-G550FY', 'SM-G550T', 'SM-G550T1', 'SM-G550T2', 'SM-G5510', 'SM-G5520',
+            'SM-G5528', 'SM-G570', 'SM-G5700', 'SM-G570F', 'SM-G570M', 'SM-G570Y',
+            'SM-G6000', 'SM-G600F', 'SM-G600F', 'SM-G600FY', 'SM-G600S', 'SM-G6100',
+            'SM-G610F', 'SM-G610FD', 'SM-G610K', 'SM-G610L', 'SM-G610M', 'SM-G610S',
+            'SM-G610Y', 'SM-G611F', 'SM-G611FF', 'SM-G611FFDD', 'SM-G611K', 'SM-G611L',
+            'SM-G611M', 'SM-G611M/DS', 'SM-G611MT', 'SM-G611S', 'SM-G615F', 'SM-G615FU',
+            'SM-G6200', 'SM-G710', 'SM-G7102', 'SM-G7102T', 'SM-G7105', 'SM-G7105H',
+            'SM-G7105K', 'SM-G7105L', 'SM-G7106', 'SM-G7108', 'SM-G7108V', 'SM-G7109',
+            'SM-G710L', 'SM-G710S', 'SM-G710x', 'SM-G715F', 'SM-G715FN', 'SM-G715U',
+            'SM-G715U1', 'SM-G715W', 'SM-G715X', 'SM-G7200', 'SM-G7202', 'SM-G720AX',
+            'SM-G720N0', 'SM-G730A', 'SM-G730V', 'SM-G730W8', 'SM-G7508Q', 'SM-G7509',
+            'SM-G750A', 'SM-G750H', 'SM-G770F', 'SM-G770U1', 'SM-G780F', 'SM-G780G',
+            'SM-G780X', 'SM-G7810', 'SM-G781B', 'SM-G781BR', 'SM-G781N', 'SM-G781U',
+            'SM-G781U1', 'SM-G781V', 'SM-G800A', 'SM-G800F', 'SM-G800H', 'SM-G800M',
+            'SM-G800R4', 'SM-G800Y', 'SM-G820A', 'SM-G8508S', 'SM-G850A', 'SM-G850F',
+            'SM-G850FQ', 'SM-G850K', 'SM-G850M', 'SM-G850S', 'SM-G850W', 'SM-G850X',
+            'SM-G850Y', 'SM-G860P', 'SM-G870A', 'SM-G870D', 'SM-G870F', 'SM-G870F0',
+            'SM-G870W', 'SM-G8750', 'SM-G8850', 'SM-G8858', 'SM-G885F', 'SM-G885K',
+            'SM-G885L', 'SM-G885S', 'SM-G885X', 'SM-G885Y', 'SM-G8870', 'SM-G8870',
+            'SM-G887F', 'SM-G887N', 'SM-G888N0', 'SM-G889A', 'SM-G889G', 'SM-G890A',
+            'SM-G891', 'SM-G891A', 'SM-G892A', 'SM-G892A', 'SM-G892U', 'SM-G9006V',
+            'SM-G9008V', 'SM-G9009D', 'SM-G900A', 'SM-G900AZ', 'SM-G900F', 'SM-G900FD',
+            'SM-G900FQ', 'SM-G900H', 'SM-G900I', 'SM-G900J', 'SM-G900K', 'SM-G900L',
+            'SM-G900M', 'SM-G900MD', 'SM-G900P', 'SM-G900R4', 'SM-G900R6', 'SM-G900R7',
+            'SM-G900S', 'SM-G900T', 'SM-G900T1', 'SM-G900T3', 'SM-G900V', 'SM-G900W8',
+            'SM-G901F', 'SM-G903M', 'SM-G903W', 'SM-G906K', 'SM-G906L', 'SM-G906S',
+            'SM-G906SKL', 'SM-G9092', 'SM-G9098', 'SM-G9198', 'SM-G9200', 'SM-G9208',
+            'SM-G9209', 'SM-G9209', 'SM-G920A', 'SM-G920AZ', 'SM-G920F', 'SM-G920FQ',
+            'SM-G920G1', 'SM-G920I', 'SM-G920K', 'SM-G920L', 'SM-G920P', 'SM-G920R4',
+            'SM-G920R6', 'SM-G920R7', 'SM-G920S', 'SM-G920T', 'SM-G920T1', 'SM-G920V',
+            'SM-G920W8', 'SM-G920X', 'SM-G925', 'SM-G9250', 'SM-G925A', 'SM-G925F',
+            'SM-G925FQ', 'SM-G925I', 'SM-G925ID', 'SM-G925K', 'SM-G925L', 'SM-G925P',
+            'SM-G925R4', 'SM-G925R6', 'SM-G925R7', 'SM-G925S', 'SM-G925T', 'SM-G925V',
+            'SM-G925W8', 'SM-G925X', 'SM-G925X', 'SM-G925Z', 'SM-G9280', 'SM-G9287',
+            'SM-G9287C', 'SM-G928A', 'SM-G928C', 'SM-G928F', 'SM-G928G', 'SM-G928i',
+            'SM-G928K', 'SM-G928L', 'SM-G928N', 'SM-G928N0', 'SM-G928P', 'SM-G928R4',
+            'SM-G928S', 'SM-G928T', 'SM-G928V', 'SM-G928W8', 'SM-G928X', 'SM-G9298',
+            'SM-G9300', 'SM-G9308', 'SM-G930A', 'SM-G930AZ', 'SM-G930F', 'SM-G930FD',
+            'SM-G930K', 'SM-G930L', 'SM-G930P', 'SM-G930R4', 'SM-G930R6', 'SM-G930R7',
+            'SM-G930S', 'SM-G930SKL', 'SM-G930T', 'SM-G930T1', 'SM-G930U', 'SM-G930V',
+            'SM-G930VC', 'SM-G930VL', 'SM-G930W', 'SM-G930W8', 'SM-G930X', 'SM-G9350',
+            'SM-G935A', 'SM-G935AU', 'SM-G935D', 'SM-G935F', 'SM-G935FD', 'SM-G935J',
+            'SM-G935K', 'SM-G935L', 'SM-G935P', 'SM-G935R4', 'SM-G935R6', 'SM-G935R7',
+            'SM-G935S', 'SM-G935T', 'SM-G935T1', 'SM-G935U', 'SM-G935V', 'SM-G935VC',
+            'SM-G935W', 'SM-G935W8', 'SM-G935X', 'SM-G950', 'SM-G9500', 'SM-G9508',
+            'SM-G950D', 'SM-G950F', 'SM-G950FD', 'SM-G950J', 'SM-G950N', 'SM-G950U',
+            'SM-G950U1', 'SM-G950W', 'SM-G950X', 'SM-G950XC', 'SM-G955', 'SM-G9550',
+            'SM-G9558', 'SM-G955F', 'SM-G955FD', 'SM-G955J', 'SM-G955N', 'SM-G955U',
+            'SM-G955U1', 'SM-G955W', 'SM-G955X', 'SM-G955XU', 'SM-G9600', 'SM-G9608',
+            'SM-G960F', 'SM-G960FD', 'SM-G960L', 'SM-G960N', 'SM-G960U', 'SM-G960U1',
+            'SM-G960US', 'SM-G960UX', 'SM-G960W', 'SM-G960X', 'SM-G960XU', 'SM-G9650',
+            'SM-G965F', 'SM-G965FD', 'SM-G965J', 'SM-G965N', 'SM-G965U', 'SM-G965U1',
+            'SM-G965UX', 'SM-G965W', 'SM-G965X', 'SM-G965XU', 'SM-G9700', 'SM-G9708',
+            'SM-G970F', 'SM-G970FD', 'SM-G970N', 'SM-G970U', 'SM-G970U1', 'SM-G970W',
+            'SM-G970X', 'SM-G970XC', 'SM-G970XN', 'SM-G970XU', 'SM-G9730', 'SM-G9730Z',
+            'SM-G9738', 'SM-G973C', 'SM-G973D', 'SM-G973F', 'SM-G973J', 'SM-G973N',
+            'SM-G973U', 'SM-G973U1', 'SM-G973W', 'SM-G973XC', 'SM-G973XN', 'SM-G973XU',
+            'SM-G9750', 'SM-G9758', 'SM-G9758', 'SM-G975F', 'SM-G975FD', 'SM-G975N',
+            'SM-G975U', 'SM-G975U1', 'SM-G975W', 'SM-G975XC', 'SM-G975XN', 'SM-G975XU',
+            'SM-G977B', 'SM-G977N', 'SM-G977P', 'SM-G977T', 'SM-G977U', 'SM-G97xF',
+            'SM-G980A', 'SM-G980F', 'SM-G9810', 'SM-G9810-FIX', 'SM-G981A', 'SM-G981B',
+            'SM-G981C', 'SM-G981N', 'SM-G981U', 'SM-G981U1', 'SM-G981V', 'SM-G981W',
+            'SM-G985F', 'SM-G985X', 'SM-G9860', 'SM-G986B', 'SM-G986N', 'SM-G986U',
+            'SM-G986U1', 'SM-G986W', 'SM-G9880', 'SM-G9880-FIX', 'SM-G988B', 'SM-G988BR',
+            'SM-G988N', 'SM-G988U', 'SM-G988U1', 'SM-G988W', 'SM-G990B', 'SM-G990B2',
+            'SM-G990E', 'SM-G990U', 'SM-G990U-FIX', 'SM-G990U1', 'SM-G990U2', 'SM-G9910',
+            'SM-G991B', 'SM-G991BR', 'SM-G991N', 'SM-G991XU', 'SM-G9960', 'SM-G9968',
+            'SM-G996B', 'SM-G996B-FIX', 'SM-G996BR', 'SM-G996N', 'SM-G996X', 'SM-G996XU',
+            'SM-G9980', 'SM-G9988', 'SM-G998B', 'SM-G998N', 'SM-G998X', 'SM-G998XU',
+            'SM-J730F', 'SM-M017F'
+        )
+
+class IOS():
+    """
+    iOS mobile operating system
+    
+    description:
+        The version of iOS is based on the build number. We have used the stable release version provided by Apple to determine the minor version range.
+        
+    Reference:
+        https://ipsw.me/product/iPhone
+    """
+    def __init__(self):
+        pass
+    
+    def get_versions(self): 
+        return {
+            '14.0': {'minor_range': (0, 1)},
+            '14.1': {'minor_range': (0, 0)},
+            '14.2': {'minor_range': (0, 1)},
+            '14.3': {'minor_range': (0, 0)},
+            '14.4': {'minor_range': (0, 2)},
+            '14.5': {'minor_range': (0, 1)},
+            '14.6': {'minor_range': (0, 0)},
+            '14.7': {'minor_range': (0, 1)},
+            '15.0': {'minor_range': (0, 2)},
+            '15.1': {'minor_range': (0, 1)},
+            '15.2': {'minor_range': (0, 1)},
+            '15.3': {'minor_range': (0, 1)},
+            '15.4': {'minor_range': (0, 1)},
+            '15.5': {'minor_range': (0, 0)},
+            '15.6': {'minor_range': (0, 1)},
+            '16.4': {'minor_range': (0, 1)},
+            '16.5': {'minor_range': (0, 2)},
+            '16.6': {'minor_range': (0, 1)},
+            '17.0': {'minor_range': (0, 3)},
+            '17.1': {'minor_range': (0, 2)},
+            '17.2': {'minor_range': (0, 1)},
+            '17.3': {'minor_range': (0, 1)},
+            '17.4': {'minor_range': (0, 1)},
+            '17.5': {'minor_range': (0, 1)},
+        }
+
+class macOS():
+    """
+    macOS desktop operating system
+    
+    description:
+        The version of macOS is based on the build number. We have used the stable release version provided by Apple.
+        
+    Reference:
+        https://support.apple.com/en-us/HT201260
+    """
+    def __init__(self):
+        pass
+    
+    def get_versions(self): 
+        return {
+            '10.11': {'minor_range': (0, 6)},
+            '10.12': {'minor_range': (0, 6)},
+            '10.13': {'minor_range': (0, 6)},
+            '10.14': {'minor_range': (0, 6)},
+            '10.15': {'minor_range': (0, 7)},
+            '11.0': {'minor_range': (0, 0)},
+            '11.2': {'minor_range': (0, 3)},
+            '11.3': {'minor_range': (0, 1)},
+            '11.5': {'minor_range': (0, 2)},
+            '11.6': {'minor_range': (0, 6)},
+            '12.0': {'minor_range': (0, 1)},
+            '12.2': {'minor_range': (0, 1)},
+            '12.3': {'minor_range': (0, 1)},
+            '12.4': {'minor_range': (0, 0)},
+            '12.5': {'minor_range': (0, 1)},
+            '12.6': {'minor_range': (0, 4)},
+            '12.7': {'minor_range': (0, 5)},
+            '13.0': {'minor_range': (0, 1)},
+            '13.1': {'minor_range': (0, 0)},
+            '13.2': {'minor_range': (0, 1)},
+            '13.3': {'minor_range': (0, 1)},
+            '13.4': {'minor_range': (0, 1)},
+            '13.5': {'minor_range': (0, 2)},
+            '14.0': {'minor_range': (0, 1)},
+            '14.1': {'minor_range': (0, 2)},
+            '14.2': {'minor_range': (0, 1)},
+            '14.3': {'minor_range': (0, 1)},
+            '14.4': {'minor_range': (0, 1)},
+            '14.5': {'minor_range': (0, 0)},
+        }
+
+class Linux():
+    """
+    Linux desktop operating system
+    
+    description:
+        The version of Linux is based on the kernel version. We have used the stable release version provided by the Linux community.
+        
+    Reference:
+        https://www.kernel.org/
+    """
+    def __init__(self):
+        pass
+    
+    def get_versions(self): 
+        return {
+            '5.0': {'minor_range': (0, 21)},
+            '5.1': {'minor_range': (0, 21)},
+            '5.2': {'minor_range': (0, 20)},
+            '5.3': {'minor_range': (0, 18)},
+            '5.4': {'minor_range': (0, 184)},
+            '5.5': {'minor_range': (0, 19)},
+            '5.6': {'minor_range': (0, 19)},
+            '5.7': {'minor_range': (0, 19)},
+            '5.8': {'minor_range': (0, 18)},
+            '5.9': {'minor_range': (0, 16)},
+            '5.10': {'minor_range': (0, 105)},
+            '5.11': {'minor_range': (0, 22)},
+            '5.12': {'minor_range': (0, 19)},
+            '5.13': {'minor_range': (0, 19)},
+            '5.14': {'minor_range': (0, 21)},
+            '5.15': {'minor_range': (0, 103)},
+            '5.16': {'minor_range': (0, 20)},
+            '5.17': {'minor_range': (0, 15)},
+            '5.18': {'minor_range': (0, 19)},
+            '5.19': {'minor_range': (0, 17)},
+            '6.0': {'minor_range': (0, 19)},
+            '6.1': {'minor_range': (0, 78)},
+            '6.2': {'minor_range': (0, 16)},
+            '6.3': {'minor_range': (0, 13)},
+            '6.4': {'minor_range': (0, 16)},
+            '6.5': {'minor_range': (0, 13)},
+            '6.6': {'minor_range': (0, 17)},
+            '6.7': {'minor_range': (0, 5)},
+        }
+    
+class Windows():
+    """
+    Windows desktop operating system
+    
+    description:
+        The version of Windows is based on the build number. We have used the stable release version provided by Microsoft.
+        
+    Reference:
+        https://support.microsoft.com/en-us/help/12387/windows-10-update-history
+    """
+    def __init__(self):
+        pass
+    
+    def get_versions(self): 
+        return {
+            '6.1': {},
+            '6.2': {},
+            '6.3': {},
+            '10.0': {},
+        }
+        
+        
+class Mobile():
+    def get_types(self):
+        return ['Android', 'iOS']
+
+class Desktop():
+    def get_types(self):
+        return ['Windows', 'macOS', 'Linux']
+
+class Platform():
+    def __init__(self, platform_type):
+        self.platform = self.get_platform(platform_type)
+    
+    def get_platform(self, platform_type):
+        if platform_type.lower() == 'mobile':
+            return Mobile()
+        elif platform_type.lower() == 'desktop':
+            return Desktop()
+        else:
+            return
+    
+    def get_platform_versions(self):
+        return  self.platform.get_versions()
