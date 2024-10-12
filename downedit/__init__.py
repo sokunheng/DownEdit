b965097d6aa19cc247fd74c630ec7e5bb12c948e
sokunheng <snamheng@gmail.com>
2024-10-12 23:22:51 +0700

    Update

--- downedit/__init__.py
+++ downedit/__init__.py
@@ -4,11 +4,12 @@ from .__config__ import (
     Config,
     EditFolder,
     Media,
-    Extensions
+    Extensions,
+    AIContext
 )
 
 __author__ = "sokunheng"
-__version__ = "2.5.0"
+__version__ = "2.6.0"
 __description_en__ = "Download, Edit, and Generate Videos, Images and Sounds, in bulk using AI"
 __reponame__ = "DownEdit"
 __repourl__ = "https://github.com/sokunheng/DownEdit"
