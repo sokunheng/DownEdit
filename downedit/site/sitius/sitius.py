b5620e6b93f4f1c8ce6645e409237884f059bffb
sokunheng <snamheng@gmail.com>
2024-10-13 01:32:54 +0700

    init

---
+++ downedit/site/sitius/sitius.py
@@ -0,0 +1,102 @@
+import httpx
+import asyncio
+
+from downedit import AIContext
+from downedit.service import Client
+from downedit.site.domain import Domain
+from downedit.utils import (
+    log
+)
+
+class Sitius:
+    def __init__(
+        self,
+        service: Client,
+        context: AIContext = AIContext()
+    ):
+        self.service = service
+        self.context = context
+
+    async def request_job(self):
+        try:
+            model = self.context.get("model")
+            cfg_scale = self.context.get("cfg_scale")
+            steps = self.context.get("steps")
+            sampler = self.context.get("sampler")
+
+            if not model: self.context.load({"model": "StableDiffusion_v1.4"})
+            if not cfg_scale: self.context.load({"cfg_scale": 7})
+            if not steps: self.context.load({"steps": 28})
+            if not sampler: self.context.load({"sampler": "DPM++ 2M Karras"})
+
+            response = await self.service.aclient.post(
+                url=Domain.SITIUS.GENERATE,
+                json=self.context.json(),
+            )
+            response.raise_for_status()
+            return response.json()
+
+        except (
+            httpx.TimeoutException,
+            httpx.NetworkError,
+            httpx.HTTPStatusError,
+            httpx.ProxyError,
+            httpx.UnsupportedProtocol,
+            httpx.StreamError,
+            Exception
+        ) as e:
+            log.error(f"Error during job request: {e}")
+            return None
+
+    async def poll_status(self, job_id, retry_interval=3, max_retries=20):
+        """
+        Poll the server to check if the image generation is completed.
+        """
+        for attempt in range(max_retries):
+            try:
+                response = await self.service.aclient.get(
+                    f"{Domain.SITIUS.IMAGE}/{job_id}"
+                )
+                if response.status_code == 200:
+                    return response.json()
+                else:
+                    await asyncio.sleep(retry_interval)
+
+            except (
+                httpx.TimeoutException,
+                httpx.NetworkError,
+                httpx.HTTPStatusError,
+                httpx.ProxyError,
+                httpx.UnsupportedProtocol,
+                httpx.StreamError,
+                Exception
+            ) as e:
+                log.error(f"Error while fetching image: {e}")
+        return None
+
+    async def generate_image(self):
+        """
+        Generate an image using the Sitius API.
+
+        Returns:
+            str: The URL of the generated image.
+        """
+        try:
+            job_id = await self.request_job()
+            if not job_id: return None
+
+            image_url = await self.poll_status(job_id)
+            if not image_url: return None
+            return image_url
+
+        except (
+            httpx.TimeoutException,
+            httpx.NetworkError,
+            httpx.HTTPStatusError,
+            httpx.ProxyError,
+            httpx.UnsupportedProtocol,
+            httpx.StreamError,
+            Exception
+        ) as e:
+            log.error(f"Error during image generation: {e}")
+            return None
