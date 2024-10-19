a86e126f952448294e6626c8b253fc6c3cac9760
sokunheng <snamheng@gmail.com>
2024-10-19 11:47:25 +0700

    init

--- downedit/utils/__init__.py
+++ downedit/utils/__init__.py
@@ -3,7 +3,7 @@ from .system            import pc_info
 from .tool_selector     import selector
 from .singleton         import Singleton
 from .observer          import Observer
-from .resource        import ResourceUtil
+from .resource          import ResourceUtil
 from .console           import (
                             Console as console,
                             Column as column
