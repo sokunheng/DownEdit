61f2e685775d483173e27696012aa40a6f102fd1
sokunheng <snamheng@gmail.com>
2024-10-21 23:53:51 +0700

    init

---
+++ downedit/cli/menu/sounds/ai_gen_sound.py
@@ -0,0 +1,39 @@
+import time
+
+from colorama import Fore
+
+from .. import get_banner
+from downedit.utils import (
+    log,
+    selector
+)
+
+def cloud_ai_generator():
+    return None
+
+def local_ai_generator():
+    return None
+
+def display_menu():
+    banner_display, banner_msg = get_banner("AI_SOUND_EDITOR")
+    selector.display_banner(
+        banner_display,
+        banner_msg, "- ai generative"
+    )
+    available_tools = {
+        f" Cloud {Fore.RED}(Soon)" : lambda: None,
+        f" Local {Fore.RED}(Soon)" : lambda: None,
+        f" Back"                   : lambda: None,
+    }
+    return selector.start(
+        menu_options=available_tools,
+        input_message=f"{Fore.YELLOW}Select Tools{Fore.WHITE}"
+    )
+
+def main():
+    selector.run(
+        display_menu
+    )
+
+if __name__ == "__main__":
+    main()
