b83c5e6ae5441bc17be67a91cec8024fbc526be9
sokunheng <snamheng@gmail.com>
2024-10-27 21:06:47 +0700

    fd

--- downedit/edit/ai/cloud/image/generator/__init__.py
+++ downedit/edit/ai/cloud/image/generator/__init__.py
@@ -15,6 +15,7 @@ class OperationFactory:
             AIImgGenOperation: The image generation operation instance.
         """
         operations = {
+            "test"
         }
         operation_class = operations.get(operation_name.lower())
         if operation_class is None:
