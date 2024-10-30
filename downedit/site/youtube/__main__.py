c6695c8e7089fa735d2b993a2845f6c3ad5fcdf1
sokunheng <44894784+sokunheng@users.noreply.github.com>
2024-10-31 00:39:19 +0700

    init

---
+++ downedit/site/youtube/__main__.py
@@ -0,0 +1,18 @@
+
+import os
+import time
+
+from colorama import Fore
+
+from downedit.utils import (
+    ResourceUtil,
+    log,
+    selector
+)
+
+def main():
+    None
+
+
+if __name__ == "__main__":
+    main()
